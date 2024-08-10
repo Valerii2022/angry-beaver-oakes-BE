c18fb94da34aae8c7e916fea01c8ee5dab891a03
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-10 19:46:52 +0300

    update

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+// import mongoose from "mongoose";
 
-require("dotenv").config();
+import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-// Middleware
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect(process.env.DB_HOST, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
 });
