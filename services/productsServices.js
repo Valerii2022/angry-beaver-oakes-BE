a700dc79f9fd50ce18dfb3f70fd90317561c8bd2
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-11 09:36:11 +0300

    added Product schema

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,11 +1,14 @@
-import fs from "fs/promises";
-import path from "path";
+// import fs from "fs/promises";
+// import path from "path";
 
-const productsPath = path.resolve("services", "products.json");
+import Product from "../models/Product.js";
+
+// const productsPath = path.resolve("services", "products.json");
 
 const getAllProducts = async () => {
-  const products = await fs.readFile(productsPath);
-  return JSON.parse(products);
+  //   const products = await fs.readFile(productsPath);
+  //   return JSON.parse(products);
+  return Product.find();
 };
 
 export default getAllProducts;
