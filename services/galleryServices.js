bb688a2fa58ac3cc74c9d1a3f35cacafd3015853
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 17:19:13 +0300

    test mongoose

--- services/galleryServices.js
+++ services/galleryServices.js
@@ -1,14 +1,14 @@
-import fs from "fs/promises";
-import path from "path";
-// import Gallery from "../models/Gallery.js";
+// import fs from "fs/promises";
+// import path from "path";
+import Gallery from "../models/Gallery.js";
 
-// export const getGallery = async () => Gallery.find();
+export const getGallery = async () => Gallery.find();
 
-const galleryPath = path.resolve("data", "gallery.json");
+// const galleryPath = path.resolve("data", "gallery.json");
 
-const getGallery = async () => {
-  const gallery = await fs.readFile(galleryPath);
-  return JSON.parse(gallery);
-};
+// const getGallery = async () => {
+//   const gallery = await fs.readFile(galleryPath);
+//   return JSON.parse(gallery);
+// };
 
 export default getGallery;
