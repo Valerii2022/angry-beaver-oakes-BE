0f679d7d9548fb736569c87cad79a7f99228085e
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 19:48:59 +0300

    update

--- services/ordersServices.js
+++ services/ordersServices.js
@@ -1,9 +1,3 @@
-// import fs from "fs/promises";
-// import { nanoid } from "nanoid";
-import path from "path";
-
-// const orderPath = path.resolve("data", "orders.json");
-
 const addOrder = async () => {
   return "Order logic need to be here!";
 };
--- services/subscribeServices.js
+++ services/subscribeServices.js
@@ -1,9 +1,3 @@
-// import fs from "fs/promises";
-// import { nanoid } from "nanoid";
-import path from "path";
-
-const subscribePath = path.resolve("data", "subscribe.json");
-
 const addSubscribe = async () => {
   return "subscribe logic need to be here!";
 };
