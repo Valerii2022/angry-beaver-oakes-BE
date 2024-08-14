0ec5fb5b00282c499e629f670706201456ec77b8
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 23:31:49 +0300

    update product fetch logic

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,15 +1,5 @@
-import fs from "fs/promises";
-import path from "path";
+import Product from "../models/Product.js";
 
-// import Product from "../models/Product.js";
-
-// const getAllProducts = async () => Product.find();
-
-const productsPath = path.resolve("data", "products.json");
-
-const getAllProducts = async () => {
-  const products = await fs.readFile(productsPath);
-  return JSON.parse(products);
-};
+const getAllProducts = async () => Product.find();
 
 export default getAllProducts;
