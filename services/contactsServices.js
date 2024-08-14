803534e2db6d393c70adacadfecf98df59db3bc6
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 19:43:38 +0300

    corrections

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,31 +1,31 @@
-// import { nanoid } from "nanoid";
-// import fs from "fs/promises";
-import Contact from "../models/Contact.js";
-// import path from "path";
+import { nanoid } from "nanoid";
+import fs from "fs/promises";
+import path from "path";
 
-// const contactsPath = path.resolve("data", "contacts.json");
+const contactsPath = path.resolve("data", "contacts.json");
 
-// const updateContacts = (contacts) => Contact.findOneAndUpdate(contacts);
-// fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+const updateContacts = (contacts) => {
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-// const getContacts = async () => Contact.find();
-// const data = await fs.readFile(contactsPath);
-// return JSON.parse(data);
+const getContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-// const addContact = async ({ name, email, phone, message }) => {
-//   const contacts = await getContacts();
-//   const newContact = {
-//     id: nanoid(),
-//     name,
-//     email,
-//     phone,
-//     message,
-//   };
-//   contacts.push(newContact);
-//   await updateContacts(contacts);
-//   return newContact;
-// };
+const addContact = async ({ name, email, phone, message }) => {
+  const contacts = await getContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+    message,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
 
-const addContact = async (data) => Contact.create(data);
 
 export default addContact;
