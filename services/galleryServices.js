9384085935def9040a136a91ba112133ca001b80
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-16 14:21:55 +0300

    added images sort

--- services/galleryServices.js
+++ services/galleryServices.js
@@ -1,5 +1,20 @@
 import Gallery from "../models/Gallery.js";
 
-export const getGallery = async () => Gallery.find().sort({ id: 1 });
+export const getGallery = async () => {
+  const images = await Gallery.find();
+
+  images.sort((a, b) => {
+    const numA = parseInt(a.id.match(/\d+$/), 10);
+    const numB = parseInt(b.id.match(/\d+$/), 10);
+
+    if (a.title.replace(/\d+$/, "") === b.title.replace(/\d+$/, "")) {
+      return numA - numB;
+    } else {
+      return a.id.localeCompare(b.id);
+    }
+  });
+
+  return images;
+};
 
 export default getGallery;
