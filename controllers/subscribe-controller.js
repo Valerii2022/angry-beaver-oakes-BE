3c52bbcacbed31641f5927f1b143750bcba86f98
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-28 10:57:53 +0300

    added message back for subscribe

--- controllers/subscribe-controller.js
+++ controllers/subscribe-controller.js
@@ -1,8 +1,20 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import sendEmail from "../helpers/sendEmail.js";
 import addSubscribe from "../services/subscribeServices.js";
 
 const add = async (req, res, next) => {
+  const { email } = req.body;
+
   const result = await addSubscribe(req.body);
+
+  const contactEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<p>It is my testing (pet project) website!</p><p>Thank you for your subscribe!</p>`,
+  };
+
+  await sendEmail(contactEmail);
+
   res.status(201).json(result);
 };
 
