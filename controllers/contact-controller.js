03aa750d2e8099ac7dc5c9c902fa59e3e2126f7b
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-28 10:46:00 +0300

    added  email to contacts logic

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,19 +1,19 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
-// import sendEmail from "../helpers/sendEmail.js";
+import sendEmail from "../helpers/sendEmail.js";
 import addContact from "../services/contactsServices.js";
 
 const add = async (req, res, next) => {
-  // const { email } = req.body;
+  const { email } = req.body;
 
   const result = await addContact(req.body);
 
-  // const contactEmail = {
-  //   to: email,
-  //   subject: "Verify email",
-  //   html: `<p>Thank you for your message!</p>`,
-  // };
+  const contactEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<p>Thank you for your message!</p>`,
+  };
 
-  // await sendEmail(contactEmail);
+  await sendEmail(contactEmail);
 
   res.status(201).json(result);
 };
